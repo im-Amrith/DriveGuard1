4029f7a4af627cbfef086e155ed20b26c2c5f1ff
im-Amrith <amrithesh23@gmail.com>
2025-10-19 18:36:42 +0530

    app update

--- backend/app.py
+++ backend/app.py
@@ -172,5 +172,7 @@ def delete_trip(current_user, trip_id):
 
 # --- Main Entry Point ---
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
